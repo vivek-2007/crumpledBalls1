2c9977f2d675f8ebd2c3cc8efdc7fea48379cb19
vivek-2007 <83000441+vivek-2007@users.noreply.github.com>
2021-05-19 07:16:18 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,35 @@
+class Paper
+{
+	   constructor(x, y) {
+          var options = {
+              isStatic:false,
+              'restitution':0.3,
+              'friction':0,
+              'density':1.2
+               
+          }
+         
+          this.body = Bodies.rectangle(x, y, 50 ,50, options);
+          this.width = 50;
+          this.height = 50;
+          this.image = loadImage("paper.png"); 
+          World.add(world, this.body);
+        }
+        display(){
+          var pos =this.body.position;
+          
+          var angle = this.body.angle;
+    
+          push();
+          translate(pos.x, pos.y);
+          rotate(angle);
+         
+          imageMode(CENTER);
+          image(this.image, 0, 0, this.width, this.height);
+          
+          pop();
+        }
+
+     
+
+      };
